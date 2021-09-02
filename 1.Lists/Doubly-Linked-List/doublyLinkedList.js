9bc1309b9f0880192e83ac154097b853f4bea958
Jeremiah Peoples <Jeremiah.Peoples5@gmail.com>
2021-09-01 20:47:28 -0500

    Big O of SLL. Add DLL push

---
+++ 1.Lists/Doubly-Linked-List/doublyLinkedList.js
@@ -0,0 +1,29 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+  constructor(){
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+  }
+  
+  push(val){
+    var newNode = new Node(val);
+    
+    if(this.length === 0){
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++
+  }
+}
