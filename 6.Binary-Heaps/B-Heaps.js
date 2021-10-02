4375abb2dbc060c52ac2e06fbee62f5a72de5f55
Jeremiah Peoples <jeremiah.peoples5@gmail.com>
2021-10-02 17:55:00 -0500

    Did not finish heaps. Please come again soon :(

--- 6.Binary-Heaps/B-Heaps.js
+++ 6.Binary-Heaps/B-Heaps.js
@@ -11,7 +11,6 @@ class MaxBinaryHeap{
   bubbleUp(){
     let index = this.values.length -1;
     const element = this.values[index];
-    
     while(index > 0){
       let parentIndex = Math.floor((index-1)/2)
       let parent = this.values[parentIndex];
@@ -22,9 +21,38 @@ class MaxBinaryHeap{
        index = parentIndex;
       }
   }
+  
+  extractMax(){
+    const originalRoot = this.root;
+    const end = this.values.pop();
+    this.values[0] = end 
+    
+    this.sinkDown();
+    
+    return originalRoot;
+  }
+  sinkDown(){
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0]
+    
+    while(true){
+      let leftChildIndex = 2 *index + 1
+      let rightChildIndex = 2* index + 2;
+      let leftChildVal;
+      let rightChildVal;
+      let swap = null;
+      
+      if(leftChildIndex < length){
+        leftChild = this.values[leftChildIndex]
+      }
+    }
+      if (swap === null)break
+  }
 }
 
 var MBH = new MaxBinaryHeap();
+
 
 MBH.insert(41)
 MBH.insert(39)
@@ -35,4 +63,6 @@ MBH.insert(12)
 MBH.insert(55)
 MBH.insert(100)
 MBH.values
+
+
 
